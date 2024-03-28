8e8b310fed76e3743b67b0cbb6905dbc9bdf3874
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-03-28 19:55:49 +0700

    Time: 6 ms (89.55%), Space: 13.4 MB (49.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> mp;
         map<int, int>::iterator m = mp.end();
+        int t = nums.size();
         int t2, i;
-        for (i = 0; i < nums.size(); ++i) {
+        for (i = 0; i < t; ++i) {
             t2 = target - nums[i];
             if (mp.find(t2) != m) {
                 break;
