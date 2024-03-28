91c8c995ab71e51b90360b79b7c9b3c0e4661a64
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-03-28 21:04:24 +0700

    Time: 16 ms (80.22%), Space: 12.9 MB (86.66%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -3,7 +3,7 @@ public:
     bool repeatedSubstringPattern(string s) {
         string init=s;
         s+=s;
-        int n=s.size(),t=init.size();        
+        int n=s.size();        
         s[0]=' ';
         s[n-1]=' ';
         if(s.find(init)!=string::npos) return true;
