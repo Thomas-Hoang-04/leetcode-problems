b57cd508e95806d3970b5e6044dae9ded0a4b367
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-03-29 15:06:49 +0700

    Delete 0459-repeated-substring-pattern directory

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    bool repeatedSubstringPattern(string s) {
-        string sf = s + s;
-        return sf.substr(1, sf.length() - 2).find(s) != string::npos;
-    }
-};
