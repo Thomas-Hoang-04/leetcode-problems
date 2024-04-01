920d6a74b12faab973caeaf7ac3a005f4704c05b
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-04-01 17:39:42 +0700

    Delete 0058-length-of-last-word directory

--- 0058-length-of-last-word/0058-length-of-last-word.c
+++
@@ -1,7 +0,0 @@
-int lengthOfLastWord(char* s) {
-    int i = strlen(s) - 1, count = 0;
-    while (i >= 0 && s[i--] == ' ');
-    count++;
-    while (i >= 0 && s[i--] != ' ') count++;
-    return count;
-}
