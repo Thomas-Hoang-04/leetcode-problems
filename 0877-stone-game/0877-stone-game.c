e8a12a92c24583fb4892638f08b404c5354a0b37
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-04-04 15:38:17 +0700

    Time: 0 ms (100%), Space: 5.5 MB (80.43%) - LeetHub

--- 0877-stone-game/0877-stone-game.c
+++ 0877-stone-game/0877-stone-game.c
@@ -1,3 +1,7 @@
 bool stoneGame(int* piles, int pilesSize) {
     return true;
+    /* Alice goes first, so she can always choose all of the odd piles or the even piles
+    If Alice choose all odd piles, and sum(piles[odd]) > sum(piles[even]) -> Win
+    If Alice choose all even piles, and sum(piles[even]) > sum(piles[odd]) -> Win
+    => Alice always win */
 }
