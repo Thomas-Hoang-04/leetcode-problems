29b7648ee777cb0d78e2f244e37528ea4e760de0
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-04-11 16:07:43 +0700

    Time: 10 ms (66.02%), Space: 9.8 MB (86.82%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        if (num.length() == k) return "0";
+        if (num.size() == k) return "0";
         string ans = "";
         int i = 0;
-        for (i; i < num.length(); ++i) {
-            while (k > 0 && ans.length() > 0 && ans.back() > num[i]) {
+        for (i; i < num.size(); ++i) {
+            while (k > 0 && ans.size() > 0 && ans.back() > num[i]) {
                 ans.pop_back();
                 --k;
             }
             if (num[i] == '0' && ans.empty()) continue;
             ans += num[i];
         }
-        while (k-- > 0 && ans.length() > 0) ans.pop_back();
+        while (k-- > 0 && ans.size() > 0) ans.pop_back();
         return ans.empty() ? "0" : ans;
     }
 };
