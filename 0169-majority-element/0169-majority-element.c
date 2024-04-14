6d3e84d1556c70f6130bc8b62b8a55f3913b91d2
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-04-14 22:17:05 +0700

    Time: 21 ms (35.12%), Space: 7.5 MB (96.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,13 +1,13 @@
 int majorityElement(int* nums, int numsSize) {
-    int ans = nums[0];
+    int ans = 0;
     int c = 1;
     for (int i = 0; i < numsSize; ++i) {
-        if (nums[i] == ans) c++;
+        if (nums[i] == nums[ans]) c++;
         else c--;
-        if (!c) {
-            ans = nums[i];
+        if (c == 0) {
+            ans = i;
             c = 1;
         }
     }
-    return ans; 
+    return nums[ans]; 
 }
