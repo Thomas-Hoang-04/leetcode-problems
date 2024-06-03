d77af772ce09d2305daaecccf11aec28c99c5ff5
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-06-03 22:43:47 +0700

    Time: 3 ms (86.11%), Space: 7.1 MB (38.89%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.c
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.c
@@ -1,7 +1,6 @@
 int appendCharacters(char* s, char* t) {
-    int s_idx = 0, t_idx = 0;
-    for (s_idx; s_idx < strlen(s); s_idx++) {
-        if (s[s_idx] == t[t_idx]) t_idx++;
-    }
-    return strlen(t) - t_idx;
+    char *st = t;
+    while (*s) if (*t == *s++) if (*++t == 0) return 0;
+    int k = t - st;
+    return strlen(st) - k;
 }
