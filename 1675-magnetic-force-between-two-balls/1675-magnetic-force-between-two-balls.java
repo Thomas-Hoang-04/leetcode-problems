53fa28d7c3c2777ba4d8239397ba972c092cd211
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-06-20 15:46:07 +0700

    Time: 48 ms (6.76%), Space: 56 MB (24.25%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -1,26 +1,24 @@
 class Solution {
-    private boolean chkPlace(int x, int[] pos, int m) {
+    private int chkPlace(int x, int[] pos, int m) {
         int prev = 0;
         int ballsCnt = 1;
-        for (int i = 1; i < pos.length && ballsCnt < m; i++) {
+        for (int i = 1; i < pos.length; i++) {
             if (pos[i] - pos[prev] >= x) {
                 ballsCnt++;
                 prev = i;
             }
         }
-        return ballsCnt == m;
+        return ballsCnt;
     }
     public int maxDistance(int[] pos, int m) {
-        int maxDist = 0;
         Arrays.sort(pos);
         int low = 1, high = Arrays.stream(pos).max().getAsInt(), mid;
         while (low <= high) {
             mid = low + (high - low) / 2;
-            if (chkPlace(mid, pos, m)) {
-                maxDist = mid;
+            if (chkPlace(mid, pos, m) >= m) {
                 low = mid + 1;
             } else high = mid - 1;
         }
-        return maxDist;
+        return high;
     }
 }
