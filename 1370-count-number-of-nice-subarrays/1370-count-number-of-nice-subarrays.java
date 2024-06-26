56a3cea51528e2644fb3c10a060a880ad3485c35
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-06-26 14:21:06 +0700

    Time: 10 ms (52.82%), Space: 54.5 MB (65.97%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,13 +1,18 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        int curr = 0, subs = 0;
-        HashMap<Integer, Integer> trackSum = new HashMap<>();
-        trackSum.put(0, 1);
-        for (int n: nums) {
-            curr += n % 2;
-            if (trackSum.get(curr - k) != null) subs += trackSum.get(curr - k);
-            trackSum.put(curr, trackSum.getOrDefault(curr, 0) + 1);
+    private int maxSub(int[] nums, int k) {
+        int curr = 0, st = 0, subs = 0;
+        for (int e = 0; e < nums.length; e++) {
+            curr += nums[e];
+            while (curr > k) {
+                curr -= nums[st];
+                st++;
+            }
+            subs += e - st + 1;
         }
         return subs;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {  
+        for (int i = 0; i < nums.length; i++) nums[i] = nums[i] % 2;   
+        return maxSub(nums, k) - maxSub(nums, k - 1);
     }
 }
