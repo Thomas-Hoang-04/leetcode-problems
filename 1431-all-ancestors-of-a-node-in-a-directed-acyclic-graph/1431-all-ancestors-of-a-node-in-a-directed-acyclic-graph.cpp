47da69de0a6a76805d06f15718bfd06a48c49ef7
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-06-29 21:40:25 +0700

    Time: 105 ms (84.3%), Space: 70 MB (67.62%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    vector<vector<int>> p;
+    vector<vector<int>> g;
+    void findParDFS(int prev, int curr) {
+        for (int adj: g[curr]) {
+            if (p[adj].empty() || p[adj].back() != prev) {
+                p[adj].push_back(prev);
+                findParDFS(prev, adj);
+            }
+        }
+    }
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        p = vector<vector<int>>(n, vector<int>());
+        g = vector<vector<int>>(n, vector<int>());
+        for (int i = 0; i < edges.size(); i++) g[edges[i][0]].push_back(edges[i][1]);
+        for (int i = 0; i < n; i++) findParDFS(i, i);
+        return p;
+    }
+};
