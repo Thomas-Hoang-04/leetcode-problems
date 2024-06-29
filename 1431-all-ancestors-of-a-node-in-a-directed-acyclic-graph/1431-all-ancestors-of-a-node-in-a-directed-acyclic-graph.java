d1271b77e82357317d64274019e8673ce26c9d86
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-06-29 21:56:30 +0700

    Time: 41 ms (98.71%), Space: 65.2 MB (68.45%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,13 +1,13 @@
 class Solution {
     private List<Integer>[] g;
     private List<List<Integer>> p;
-    private void findParDFS(int prev, int curr) {
-        for (int adj: g[curr]) {
-            if (p.get(adj).isEmpty() || p.get(adj).lastIndexOf(prev) != p.get(adj).size() - 1) {
+    private void findParDFS(int prev, int curr, boolean[] visit) {
+        visit[curr] = true;
+        for (int adj: g[curr]) 
+            if (!visit[adj]) {
                 p.get(adj).add(prev);
-                findParDFS(prev, adj);
-            }
-        }
+                findParDFS(prev, adj, visit);
+            }   
     }
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
         g = new ArrayList[n];
@@ -19,7 +19,7 @@ class Solution {
         }
 
         for (int i = 0; i < edges.length; i++) g[edges[i][0]].add(edges[i][1]);
-        for (int i = 0; i < n; i++) findParDFS(i, i);
+        for (int i = 0; i < n; i++) findParDFS(i, i, new boolean[n]);
 
         return p;
     }
