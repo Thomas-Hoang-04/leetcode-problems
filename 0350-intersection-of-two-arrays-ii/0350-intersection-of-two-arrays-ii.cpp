e9ede3950343524a5a93d08a527ca9128e945fad
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-02 11:14:24 +0700

    Time: 3 ms (78.74%), Space: 13.1 MB (47.71%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int cnt[1001] = { 0 };
+        vector<int> res;
+
+        for (int n: nums1) cnt[n]++;
+
+        for (int n: nums2) if (cnt[n]-- > 0) res.push_back(n);
+
+        return res;
+    }
+};
