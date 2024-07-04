ec7cd5abaaba9dbed9785101062a7978e7b36e02
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-04 10:48:14 +0700

    Time: 390 ms (96.16%), Space: 255.6 MB (98.71%) - LeetHub

---
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* t = head->next;
+        ListNode* ans = head;
+        int sum;
+    while (t) {
+        sum = 0;
+        while (t->val) {
+            sum += t->val;
+            t = t->next;
+        }
+        t = t->next;
+        head->val = sum;
+        if (!t) head->next = nullptr;
+        else head = head->next;
+    }
+    return ans;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
