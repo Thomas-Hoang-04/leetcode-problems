5cd99ccd8681b9616bc0ac9aae7c810c120648aa
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-05 14:39:39 +0700

    Time: 159 ms (80.78%), Space: 115.8 MB (58.25%) - LeetHub

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int minDis = INT_MAX, minPos = 0, lastCrit = minPos, currPos = 2;
+        ListNode* prev = head;
+        head = head->next;
+        while (head->next) {
+            if ((head->val > prev->val && head->val > head->next->val) || (head->val < prev->val && head->val < head->next->val)) {
+                if (minPos == 0) minPos = currPos;
+                else minDis = min(minDis, currPos - lastCrit);
+                lastCrit = currPos;
+            }
+            currPos++;
+            prev = head;
+            head = head->next;
+        } 
+        if (minDis == INT_MAX) return { -1, -1 };
+        return { minDis, lastCrit - minPos };
+    }
+};
