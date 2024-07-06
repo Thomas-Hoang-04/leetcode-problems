07cddad3d524423907249cf3bc9f5fd9a767bbe4
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-06 23:08:10 +0700

    Time: 0 ms (100%), Space: 7.1 MB (30.58%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int full = time / (n - 1);
+        int extra = time % (n - 1);
+        if (full % 2)
+            return n - extra;
+        else
+            return extra + 1;
+    }
+};
