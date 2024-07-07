909bc2a77af75de533184a34c27cc569e40558a8
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-07 21:26:32 +0700

    Time: 5 ms (5.26%), Space: 5.6 MB (10.53%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.c
@@ -0,0 +1,8 @@
+int maxBottlesDrunk(int numBottles, int numExchange) {
+    int ans = numBottles, total = numBottles;
+        while (numExchange <= total) {
+            total -= numExchange++ - 1;
+            ans++;
+        } 
+        return ans;
+}
