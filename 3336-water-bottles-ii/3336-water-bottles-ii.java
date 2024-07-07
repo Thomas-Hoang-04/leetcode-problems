4bf417d2e2975e5b0851967ed3de5a2e7e9b1487
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-07 21:25:51 +0700

    Time: 0 ms (100%), Space: 40.9 MB (21.73%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles, total = numBottles;
+        while (numExchange <= total) {
+            total -= numExchange - 1;
+            ans++;
+            numExchange++;
+        } 
+        return ans;
+    }
+}
