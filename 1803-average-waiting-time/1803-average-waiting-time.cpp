f41d5f6d97b72c428ece768daac00ec78e263b46
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-09 16:15:39 +0700

    Time: 142 ms (79.41%), Space: 76.6 MB (35%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int curr_time = -1;
+        double wait_time = 0;
+        for (vector<int>& time: customers) {
+            if (curr_time < time[0]) {
+                wait_time += time[1];
+                curr_time = time[0] + time[1];
+            }
+            else {
+                curr_time += time[1];
+                wait_time += curr_time - time[0];
+            }
+        }   
+        return wait_time / customers.size();
+    }
+};
