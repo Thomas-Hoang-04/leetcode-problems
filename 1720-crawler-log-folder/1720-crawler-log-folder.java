b8fe332cbdaa3dfb0d61c612c71fe7afdfb98249
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-12 14:06:49 +0700

    Time: 1 ms (98.78%), Space: 41.3 MB (99.27%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,10 +1,11 @@
 class Solution {
-    private int pos(String log, int ops) {
+    private int ops;
+    private int pos(String log) {
         return log.charAt(1) == '.' ? ops > 0 ? -1 : 0 : log.charAt(0) == '.' ? 0 : 1;
     }
     public int minOperations(String[] logs) {
-        int ops = 0;
-        for (String log: logs) ops += pos(log, ops);
+        ops = 0;
+        for (String log: logs) ops += pos(log);
         return ops;
     }
 }
