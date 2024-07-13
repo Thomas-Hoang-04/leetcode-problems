e3bd079c2845c46c9262854cffee879348baa24c
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-13 13:31:43 +0700

    Time: 1 ms (100%), Space: 45.5 MB (9.45%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] asteroidCollision(int[] ast) {
+        int[] pow = new int[ast.length];
+        int curr_pow = 0;
+        boolean track;
+        for (int s: ast) {
+            if (s > 0 || (curr_pow > 0 && pow[curr_pow - 1] < 0)) pow[curr_pow++] = s;
+            else {
+                track = true;
+                while (curr_pow > 0 && pow[curr_pow - 1] > 0) {
+                    if (!track) break;
+                    if (s + pow[curr_pow - 1] <= 0) {
+                        track = !(s == -pow[curr_pow-- - 1]);
+                    } else track = false;
+                }
+                if (track) pow[curr_pow++] = s;
+            }
+        }
+        return Arrays.copyOf(pow, curr_pow);
+    }
+}
