490609d715823de5ef6036324c6b42914dca4f65
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-16 13:56:20 +0700

    Time: 533 ms (98.06%), Space: 231.1 MB (98.28%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        TreeNode* nodes[100001] = {nullptr};
+        bool child[100001] = {0};
+        bool par[100001] = {0};
+
+        for (vector<int>& node: descriptions) {
+            if (!nodes[node[0]]) nodes[node[0]] = new TreeNode(node[0]);
+            if (!nodes[node[1]]) nodes[node[1]] = new TreeNode(node[1]);
+            if (node[2]) nodes[node[0]]->left = nodes[node[1]];
+            else nodes[node[0]]->right = nodes[node[1]];
+
+            child[node[1]] = true;
+            par[node[0]] = !child[node[0]];
+            par[node[1]] = !child[node[1]];
+        }
+
+        for (int i = 1; i < 100001; i++) if (par[i]) return nodes[i];
+
+        return nullptr;
+    }
+};
