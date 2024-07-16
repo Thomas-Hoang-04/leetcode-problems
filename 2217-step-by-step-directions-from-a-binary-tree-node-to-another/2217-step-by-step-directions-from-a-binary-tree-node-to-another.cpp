6877dba71d9137e71e9ca8d83abfb7a0c26f88fa
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-16 15:39:25 +0700

    Time: 137 ms (95.26%), Space: 119.7 MB (34.86%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -9,7 +9,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#pragma GCC optimize (O3, "unroll-loops")
 #pragma GCC target ("avx")
 #pragma GCC target ("-fsplit-loops")
 
