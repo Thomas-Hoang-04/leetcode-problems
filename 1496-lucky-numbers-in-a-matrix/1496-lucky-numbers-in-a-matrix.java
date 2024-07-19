ff9e4c359d4414b93ffc312c5ece02a06c4d0650
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-19 09:15:02 +0700

    Time: 3 ms (42.69%), Space: 45.4 MB (10.55%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -1,16 +1,22 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
-        int[] rows = new int[matrix.length];
-        int[] cols = new int[matrix[0].length];
-        for (int r = 0; r < matrix.length; r++) 
-            for (int c = 0; c < matrix[0].length; c++) {
-                rows[r] = rows[r] == 0 ? matrix[r][c] : Math.min(rows[r], matrix[r][c]);
-                cols[c] = Math.max(cols[c], matrix[r][c]);
-            }
-        for (int r = 0; r < matrix.length; r++) 
+        int[] track = new int[2];
+        track[0] = Integer.MIN_VALUE;
+        track[1] = Integer.MAX_VALUE;
+        int trace;
+        for (int r = 0; r < matrix.length; r++) {
+            trace = Integer.MAX_VALUE;
             for (int c = 0; c < matrix[0].length; c++) 
-                if (matrix[r][c] == rows[r] && matrix[r][c] == cols[c]) ans.add(matrix[r][c]);
-        return ans;
+                trace = Math.min(trace, matrix[r][c]);
+            track[0] = Math.max(trace, track[0]);
+        }
+        for (int c = 0; c < matrix[0].length; c++) {
+            trace = Integer.MIN_VALUE;
+            for (int r = 0; r < matrix.length; r++) 
+                trace = Math.max(trace, matrix[r][c]);
+            track[1] = Math.min(trace, track[1]); 
+        }
+        if (track[0] == track[1]) return new ArrayList<>(Arrays.asList(track[0]));
+        return new ArrayList<>();
     }
 }
