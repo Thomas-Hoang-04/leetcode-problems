557b676a80263665a7d190df1751f01b91c47090
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-20 22:47:19 +0700

    Time: 30 ms (89.16%), Space: 36 MB (16.25%) - LeetHub

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize(O3, "unroll-loops")
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        vector<vector<int>> ans(rowSum.size(), vector<int>(colSum.size(), 0));
+
+        for (int r = 0; r < rowSum.size(); r++) 
+            for (int c = 0; c < colSum.size(); c++) {
+                ans[r][c] = min(rowSum[r], colSum[c]);
+
+                rowSum[r] -= ans[r][c];
+                colSum[c] -= ans[r][c];
+            }
+        return ans;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
