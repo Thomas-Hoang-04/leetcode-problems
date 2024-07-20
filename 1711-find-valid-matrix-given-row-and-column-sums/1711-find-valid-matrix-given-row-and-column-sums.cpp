221110c64664d40b79d4f110338dcf084107f89f
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-20 22:52:07 +0700

    Time: 40 ms (38.83%), Space: 36.3 MB (6.09%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,3 +1,7 @@
+#pragma GCC optimize(O3, "unroll-loops")
+#pragma GCC target ("avx")
+#pragma GCC target ("-fsplit-loops")
+
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
@@ -18,3 +22,9 @@ public:
     }
 };
 
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
