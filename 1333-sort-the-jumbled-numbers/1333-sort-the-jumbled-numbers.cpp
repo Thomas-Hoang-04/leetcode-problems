179cfff827ea3f1e6db9f7bc87ae56235b328c5e
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-24 20:36:08 +0700

    Time: 200 ms (71.43%), Space: 89.6 MB (46.75%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.cpp
@@ -1,3 +1,5 @@
+typedef pair<int, int> p2;
+
 class Solution {
 private:
     int create_map(int org, vector<int>& mp) {
@@ -12,12 +14,28 @@ private:
     }
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for (int n: nums) mp[n] = create_map(n, mapping);
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            return mp[a] < mp[b];
-        });
-        return nums;
+        vector<p2> mp_pair;
+        int map_val, pl, org;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!nums[i]) {
+                mp_pair.push_back({mapping[0], i});
+                continue;
+            }
+            org = nums[i];
+            map_val = 0;
+            pl = 1;
+            while (org) {
+                map_val = pl * mapping[org % 10] + map_val;
+                pl *= 10;
+                org /= 10;
+            }
+            cout << map_val << " " << i << endl;
+            mp_pair.push_back({map_val, i});
+        }
+        sort(mp_pair.begin(), mp_pair.end());
+        mapping.clear();
+        for (p2& p: mp_pair) mapping.push_back(nums[p.second]);
+        return mapping;
     }
 };
 
