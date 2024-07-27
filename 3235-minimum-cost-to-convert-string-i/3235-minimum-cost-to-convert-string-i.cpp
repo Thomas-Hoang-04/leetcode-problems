ede8b2246db1422546cadcecce8b5fda69fde71e
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-27 10:39:45 +0700

    Time: 130 ms (98.05%), Space: 99.2 MB (45%) - LeetHub

---
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize(O3, "unroll-loops")
+#pragma GCC target ("avx")
+#pragma GCC target ("-fsplit-loops")
+
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>> minDist(26, vector<long long>(26, INT_MAX));
+
+        for (int i = 0; i < 26; i++) minDist[i][i] = 0;
+
+        for (int i = 0; i < original.size(); i++) 
+            minDist[original[i] - 'a'][changed[i] - 'a'] = min((long long)cost[i], minDist[original[i] - 'a'][changed[i] - 'a']);
+
+        for (int c = 0; c < 26; c++) 
+            for (int st = 0; st < 26; st++)
+                for (int e = 0; e < 26; e++) {
+                    if (st == e) continue;
+                    minDist[st][e] = min(minDist[st][e], minDist[st][c] + minDist[c][e]);
+                }
+
+        long long minCost = 0;
+        for (int i = 0; i < source.size(); i++) {
+            if (source[i] == target[i]) continue;
+
+            if (minDist[source[i] - 'a'][target[i] - 'a'] >= INT_MAX) return -1;
+
+            minCost += minDist[source[i] - 'a'][target[i] - 'a'];
+        }
+        return minCost;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
