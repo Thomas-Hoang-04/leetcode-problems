25c036ea02b45942c64382c7bd8bad5dede2e79e
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-07-30 23:15:35 +0700

    Time: 21 ms (84.96%), Space: 45.5 MB (23.12%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -3,8 +3,8 @@ class Solution {
         int min_del = 0;
         int b_cnt = 0;
 
-        for (char c : s.toCharArray()) {
-            if (c == 'b') b_cnt++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'b') b_cnt++;
             else min_del = Math.min(min_del + 1, b_cnt);
         }
 
