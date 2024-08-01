11c04a8b454a11877a93e266db7d78d4ee538043
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-01 10:21:53 +0700

    Time: 177 ms (45%), Space: 36.9 MB (90%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.kt
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.kt
@@ -1,15 +1,7 @@
 class Solution {
     fun countSeniors(details: Array<String>): Int {
-        var cnt : Int = 0;
-        var ageT : Int;
-        var age : Int;
-        for (id in details) {
-            ageT = id[11] - '0';
-            age = id[12] - '0';
-
-            if (ageT * 10 + age > 60) cnt++;
+        return details.count {
+            id -> id[11] > '6' || (id[11] == '6' && id[12] > '0');
         }
-
-        return cnt;
     }
 }
