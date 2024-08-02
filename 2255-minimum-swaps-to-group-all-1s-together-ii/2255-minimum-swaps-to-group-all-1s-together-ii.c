2445ef45caac13c9c3de21f5dd55c8cc72919ce8
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-02 11:00:44 +0700

    Time: 69 ms (100%), Space: 14.2 MB (50%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.c
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.c
@@ -10,10 +10,7 @@ int minSwaps(int* nums, int numsSize) {
     int cnt_one = nums[0], e = 0;
     for (int st = 0; st < numsSize; st++) {
         if (st) cnt_one -= nums[st - 1];
-        while (e - st + 1 < tot_one) {
-            e++;
-            cnt_one += nums[e % numsSize];
-        }
+        while (e - st + 1 < tot_one) cnt_one += nums[++e % numsSize];
         swap_cnt = min(swap_cnt, tot_one - cnt_one);
     }
     return swap_cnt;
