a90882fe776699072dc1fdedca7e4971c3ffc3b8
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-05 20:52:15 +0700

    Time: 225 ms (7.14%), Space: 36.6 MB (85.71%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.kt
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.kt
@@ -1,12 +1,11 @@
 class Solution {
     fun kthDistinct(arr: Array<String>, k: Int): String {
-        val mp : HashMap<String, Int> = HashMap();
-        for (str in arr) mp[str] = mp.getOrElse(str) {0} + 1;
-        var kVal = k;
-        for (s in arr) {
-            if (mp.get(s) == 1) kVal--;
-            if (kVal == 0) return s;
-        }
+        val mp = LinkedHashMap<String, Int>();
+        for (str in arr) mp[str] = mp.getOrDefault(str, 0) + 1;
+        val resMp = mp.filter { (k, v) -> v == 1 };
+
+        var i = 1;
+        for (key in resMp.keys) if (i++ == k) return key;
         return "";
     }
 }
