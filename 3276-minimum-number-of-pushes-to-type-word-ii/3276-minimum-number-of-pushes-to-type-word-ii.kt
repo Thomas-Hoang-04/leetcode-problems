8558e3857d67beedce807f667908d0b9341574ee
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-06 22:33:17 +0700

    Time: 327 ms (33.33%), Space: 40.6 MB (100%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun minimumPushes(word: String): Int {
+        val letterCnt = Array<Int>(26) { 0 };
+        var maxKey : Int = 8;
+        var push : Int = 1;
+        var ans = 0;
+        for (c in word) letterCnt[c - 'a']++;
+        letterCnt.sortDescending();
+        for (c in letterCnt) {
+            if (c == 0) break;
+            ans += push * c;
+            maxKey--;
+            if (maxKey == 0) {
+                push++;
+                maxKey = 8;
+            }
+        }
+        return ans;
+    }
+}
