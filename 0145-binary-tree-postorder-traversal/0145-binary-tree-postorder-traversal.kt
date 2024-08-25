cb45378faf6bb2a09a97942dc04e8c86c72c25f8
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-25 11:21:51 +0700

    Time: 150 ms (63.82%), Space: 34.5 MB (42.21%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.kt
@@ -0,0 +1,24 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    private val ans: MutableList<Int> = mutableListOf();
+    private fun postOrd(root: TreeNode?): Unit {
+        if (root == null) return;
+
+        postOrd(root.left);
+        postOrd(root.right);
+        ans.add(root.`val`);
+    }
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+        postOrd(root);
+        return ans;
+    }
+}
