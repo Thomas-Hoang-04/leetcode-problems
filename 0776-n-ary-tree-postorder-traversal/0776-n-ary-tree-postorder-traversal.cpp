4199df276c458c790809be5cbfb7d77f0ddf9955
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-26 22:09:30 +0700

    Time: 13 ms (64.15%), Space: 15.2 MB (49.47%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if (!root) return {};
+
+        vector<int> res;
+
+        stack<Node*> trav;
+        trav.push(root);
+
+        Node* t;
+        while (!trav.empty()) {
+            t = trav.top();
+            trav.pop();
+
+            res.push_back(t->val);
+
+            for (Node* c: t->children) trav.push(c);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
