36d59cd8cd94289d398aef5deb238fa9b5ed7536
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-27 15:55:27 +0700

    Time: 436 ms (100%), Space: 56.4 MB (96.55%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.kt
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.kt
@@ -1,27 +1,27 @@
 class Solution {
+    data class Edge(val v: Int, val p: Double);
+
     fun maxProbability(n: Int, edges: Array<IntArray>, succProb: DoubleArray, st: Int, e: Int): Double {
-        val graph : Array<MutableList<Pair<Double, Int>>> = Array(n) { mutableListOf() };
+        val graph : Array<MutableList<Edge>> = Array(n) { mutableListOf<Edge>() }.apply {
+            edges.forEachIndexed { i, (u, v) -> 
+                get(u).add(Edge(v, succProb[i]));
+                get(v).add(Edge(u, succProb[i]));
+            }
+        };
 
-        for (i in 0 until edges.size) {
-            graph[edges[i][0]].add(Pair(succProb[i], edges[i][1]));
-            graph[edges[i][1]].add(Pair(succProb[i], edges[i][0]));
-        }
+        val maxProb : DoubleArray = DoubleArray(n) { 0.0 }.apply { set(st, 1.0) };
 
-        val maxProb : DoubleArray = DoubleArray(n) { 0.0 };
-        maxProb[st] = 1.0;
-
-        val pq : PriorityQueue<Pair<Double, Int>> = PriorityQueue(compareBy { it.first });
-        pq.offer(Pair(-1.0, st));
+        val pq : PriorityQueue<Edge> = PriorityQueue<Edge>(compareByDescending { it.p }).apply { offer(Edge(st, 1.0)) };
 
         while (!pq.isEmpty()) {
-            val (p, v) = pq.poll();
+            val next = pq.poll();
 
-            if (v == e) return -p;
+            if (next.v == e) return next.p;
 
-            for (pr in graph[v]) {
-                if (-p * pr.first > maxProb[pr.second]) {
-                    maxProb[pr.second] = -p * pr.first;
-                    pq.offer(Pair(-maxProb[pr.second], pr.second));
+            for (pr in graph[next.v]) {
+                if (next.p * pr.p > maxProb[pr.v]) {
+                    maxProb[pr.v] = next.p * pr.p;
+                    pq.offer(Edge(pr.v, maxProb[pr.v]));
                 }
             }
         }
