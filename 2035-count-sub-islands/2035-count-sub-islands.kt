533a059fe39678d5a630b6af4e435f65cfecd88f
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-08-28 14:46:04 +0700

    Time: 704 ms (66.67%), Space: 72.3 MB (100%) - LeetHub

---
+++ 2035-count-sub-islands/2035-count-sub-islands.kt
@@ -0,0 +1,46 @@
+class Solution {
+    data class Coor(val r: Int, val c: Int);
+    private val dirs : Array<IntArray> = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(-1, 0));
+    private lateinit var visited : Array<BooleanArray>;
+    private fun isLand(r: Int, c: Int, grid: Array<IntArray>) : Boolean {
+        return r >= 0 && c >= 0 && r < grid.size && c < grid[0].size && grid[r][c] == 1;
+    }
+    private fun isSubIsland(r: Int, c: Int, grid1: Array<IntArray>, grid2: Array<IntArray>) : Boolean {
+        var isSub : Boolean = true;
+
+        val pend : Queue<Coor> = LinkedList();
+        pend.offer(Coor(r, c));
+        visited[r][c] = true;
+
+        var t : Coor;
+        var rt: Int; var ct: Int;
+        while (!pend.isEmpty()) {
+            t = pend.poll();
+
+            if (!isLand(t.r, t.c, grid1)) isSub = false;
+
+            for (dir in dirs) {
+                rt = t.r + dir[0];
+                ct = t.c + dir[1];
+
+                if (isLand(rt, ct, grid2) && !visited[rt][ct]) {
+                    pend.offer(Coor(rt, ct));
+                    visited[rt][ct] = true;
+                }
+            }
+        }
+
+        return isSub;
+
+    }
+    fun countSubIslands(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
+        visited = Array(grid1.size) { BooleanArray(grid1[0].size) { false } };
+        var subCnt: Int = 0;
+
+        for (r in 0 until grid2.size) 
+            for (c in 0 until grid2[0].size) 
+                if (!visited[r][c] && isLand(r, c, grid2)) if (isSubIsland(r, c, grid1, grid2)) subCnt++;
+
+        return subCnt;
+    }
+}
