73d1a5ba82f0a739d726c7c70b16559f7493cf55
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-01 10:58:26 +0700

    Time: 2 ms (100%), Space: 55.4 MB (76.08%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -2,11 +2,10 @@ class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
         if (m * n != original.length) return new int[0][0];
 
-        int[][] ans = new int[m][n];
+        int[][] ans = new int[m][];
 
         for (int r = 0; r < m; r++)
-            for (int c = 0; c < n; c++)
-                ans[r][c] = original[r * n + c];
+            ans[r] = Arrays.copyOfRange(original, r * n, r * n + n);
 
         return ans;
     }
