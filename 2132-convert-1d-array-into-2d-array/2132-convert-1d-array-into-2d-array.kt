eb8d85cecfcca08ef1e32a2d3fd1b53e732b2718
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-01 10:39:15 +0700

    Time: 591 ms (14.29%), Space: 51.4 MB (90.48%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.kt
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.kt
@@ -2,13 +2,10 @@ class Solution {
     fun construct2DArray(orig: IntArray, m: Int, n: Int): Array<IntArray> {
         if (m * n != orig.size) return emptyArray();
 
-        val ans : Array<IntArray> = Array(m) { IntArray(n) };
-        var idx : Int = 0;
-
-        for (r in 0 until m) 
-            for (c in 0 until n) 
-                ans[r][c] = orig[idx++];
-
-        return ans; 
+        return Array(m) {
+            r -> IntArray(n) {
+                c -> orig[r * n + c]
+            }
+        }; 
     }
 }
