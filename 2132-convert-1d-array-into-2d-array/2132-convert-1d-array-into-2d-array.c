32f7fc0bbd2ee14d23a2e3f803d8aa93f3250524
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-01 10:47:52 +0700

    Time: 414 ms (28.57%), Space: 60.1 MB (14.29%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.c
@@ -0,0 +1,28 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** construct2DArray(int* original, int originalSize, int m, int n, int* returnSize, int** returnColumnSizes) {
+    if (m * n != originalSize) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+    
+    int** ans = (int**)malloc(m * sizeof(int*));
+    for (int r = 0; r < m; r++) ans[r] = (int*)malloc(n * sizeof(int));
+
+    *returnColumnSizes = (int*)malloc(m * sizeof(int));
+    for (int r = 0; r < m; r++) (*returnColumnSizes)[r] = n;
+
+    for (int r = 0; r < m; r++) {
+        for (int c = 0; c < n; c++) {
+            ans[r][c] = original[r * n + c];
+        }
+    }
+
+    *returnSize = m;
+
+    return ans;
+}
