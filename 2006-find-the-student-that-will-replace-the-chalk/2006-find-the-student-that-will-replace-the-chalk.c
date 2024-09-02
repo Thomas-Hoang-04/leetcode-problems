430c37961c3fabcd01a8ddbab45baff4b2fd8ae5
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-02 16:16:58 +0700

    Time: 92 ms (65.71%), Space: 15.6 MB (10%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.c
@@ -0,0 +1,12 @@
+int chalkReplacer(int* chalk, int chalkSize, int k) {
+    int sum = 0;
+    for (int i = 0; i < chalkSize; i++) {
+        if (sum > k) break;
+        sum += chalk[i];
+    }
+    k %= sum;
+    if (k == 0) return 0;
+    chalkSize = 0;
+    while (k > 0) k -= chalk[chalkSize++];
+    return k == 0 ? chalkSize : chalkSize - 1;
+}
