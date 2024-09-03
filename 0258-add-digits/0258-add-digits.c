29f0e447d0e1d3904f50e83737589ddd1b46fc39
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-03 14:09:36 +0700

    Time: 2 ms (47.04%), Space: 8.2 MB (41.64%) - LeetHub

--- 0258-add-digits/0258-add-digits.c
+++ 0258-add-digits/0258-add-digits.c
@@ -1,14 +1,13 @@
 int addDigits(int num) {
     if (num < 10) return num;
     int t;
-    while (true) {
+    while (num > 9) {
         t = 0;
         while (num) {
             t += num % 10;
             num /= 10;
         }
-        if (t < 10) return t;
         num = t;
     }
-    return 0;
+    return num;
 }
