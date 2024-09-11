bc40534d451d55d7a76f5149c95a6b0f0da9ae9f
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-11 13:41:24 +0700

    Time: 2 ms (59.31%), Space: 7.5 MB (76.96%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.c
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.c
@@ -1,15 +1,15 @@
 int minFlips(int a, int b, int c){
     if ((a | b) == c) return 0;
 
-    int count = 0;
     c ^= (a | b);
     a &= b;
+    b = 0;
     
     while (c) {
-        count += (c & 1) + (a & 1 & c);
+        b += (c & 1) + (a & 1 & c);
         a >>= 1;
         c >>= 1;
     }   
 
-    return count;
+    return b;
 }
