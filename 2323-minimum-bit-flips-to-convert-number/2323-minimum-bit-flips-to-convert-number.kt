fc3fac0a961176bd7f097058c3bfc3137537befe
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-11 13:46:14 +0700

    Time: 147 ms (23.81%), Space: 33.3 MB (57.14%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.kt
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun minBitFlips(start: Int, goal: Int): Int {
-        var ref: Int = start xor goal;
-        var cnt: Int = 0;
-
-        while (ref > 0) {
-            cnt += (ref and 1);
-            ref = ref shr 1;
-        }
-
-        return cnt;
+        return (start xor goal).countOneBits();
     }
 }
