5b9e9d65e9cef66f19aaa652ed46c980884de264
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-11 13:47:51 +0700

    Time: 0 ms (100%), Space: 40.1 MB (86.13%) - LeetHub

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        if ((a | b) == c)
+            return 0;
+
+        c ^= (a | b);
+        a &= b;
+        b = 0;
+
+        while (c > 0) {
+            b += (c & 1) + (a & 1 & c);
+            a >>= 1;
+            c >>= 1;
+        }
+
+        return b;
+    }
+}
