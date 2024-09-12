9e4a8e92fcfc495b1c8d2138edf6b96a39edbf64
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-12 11:25:52 +0700

    Time: 41 ms (82.18%), Space: 34.2 MB (8.22%) - LeetHub

---
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize(O3, "unroll-loops")
+/*#pragma GCC target ("avx")
+#pragma GCC target ("-fsplit-loops")*/
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int ans = 0;
+        bool stat, c_stat;
+        for (string wrd: words) {
+            stat = true;
+            for (char c: wrd) {
+                c_stat = false;
+                for (char ref: allowed) {
+                    c_stat = ref == c;
+                    if (c_stat) break;
+                }
+                if (!c_stat) {
+                    stat = false;
+                    break;
+                }
+            }
+
+            if (stat) ans++;
+        }
+
+        return ans;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
