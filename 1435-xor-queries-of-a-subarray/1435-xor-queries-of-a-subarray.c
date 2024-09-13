12e2d4a8d3a20995a6dfa8bbe0059f98a71ed5a5
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-13 15:47:12 +0700

    Time: 114 ms (76.19%), Space: 26 MB (85.71%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.c
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.c
@@ -4,12 +4,10 @@
 int* xorQueries(int* arr, int arrSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
     *returnSize = queriesSize;
     int* ans = (int*)malloc(queriesSize * sizeof(int));
-    int pref[arrSize];
-    pref[0] = arr[0];
-    for (int i = 1; i < arrSize; i++) pref[i] = pref[i - 1] ^ arr[i];
+    for (int i = 1; i < arrSize; i++) arr[i] ^= arr[i - 1];
     for (int i = 0; i < queriesSize; i++) {
-        ans[i] = pref[queries[i][1]];
-        if (queries[i][0] > 0) ans[i] ^= pref[queries[i][0] - 1];
+        ans[i] = arr[queries[i][1]];
+        if (queries[i][0] > 0) ans[i] ^= arr[queries[i][0] - 1];
     }
     return ans;
 }
