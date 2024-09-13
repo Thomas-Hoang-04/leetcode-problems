1e6dcb8eb281282c022b905d176bc184d165f1f3
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-13 16:12:38 +0700

    Time: 45 ms (93.91%), Space: 41.8 MB (60.93%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -5,10 +5,8 @@ public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> ans;
         for (int i = 1; i < arr.size(); i++) arr[i] ^= arr[i - 1];
-        for (vector<int>& q: queries) {
-            if (q[0] == 0) ans.push_back(arr[q[1]]);
-            else ans.push_back(arr[q[0] - 1] ^ arr[q[1]]);
-        }
+        for (vector<int>& q: queries) 
+            ans.push_back(q[0] == 0 ? arr[q[1]] : arr[q[0] - 1] ^ arr[q[1]]);
         return ans;
     }
 };
