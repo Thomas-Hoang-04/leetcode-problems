d6a0c6e7cf419c26983aa28ea3b6ad3bd70f2a09
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-15 15:23:58 +0700

    Time: 23 ms (25%), Space: 11.2 MB (50%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.c
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.c
@@ -1,24 +1,45 @@
-int max(int a, int b) {
-    return a > b ? a : b;
+#define VOWELS_SIZE 5
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+static const char vowels[VOWELS_SIZE + 1] = {1, 2, 4, 8, 16, 0};
+
+bool Isvowels(char c) {
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+    return false;
 }
 
+int Find_pos(char c) {
+    int idx = -1;
+    switch (c) {
+        case 'a':
+            idx = 0;
+            break;
+        case 'e':
+            idx = 1;
+            break;
+        case 'i':
+            idx = 2;
+            break;
+        case 'o':
+            idx = 3;
+            break;
+        case 'u':
+            idx = 4;
+            break;
+        default:
+            break;
+    }
+    return idx;
+}
+    
 int findTheLongestSubstring(char* s) {
-    if (strlen(s) == 500000) return 499995;
-    int pref = 0, ans = 0;
-    int charmp[26] = {0};
-    charmp['a' - 'a'] = 1;
-    charmp['e' - 'a'] = 2;
-    charmp['i' - 'a'] = 4;
-    charmp['o' - 'a'] = 8;
-    charmp['u' - 'a'] = 16;
-    int mp[32];
-    for (int i = 0; i < 32; i++) mp[i] = -1;
-
-    for (int i = 0; i < strlen(s); i++) {
-        pref ^= charmp[s[i] - 'a'];
-        if (mp[pref] == -1 && pref != 0) mp[pref] = i;
-
-        ans = max(ans, i - mp[pref]);
+    int n = strlen(s), ans = 0, mask = 0;
+    int arr[32];
+    memset(arr, -1, sizeof(arr));
+    for (int i = 0; i < n; i++) {
+        mask ^= vowels[(Isvowels(s[i])) ? Find_pos(s[i]) : VOWELS_SIZE];
+        if (mask != 0 && arr[mask] == -1) arr[mask] = i;
+        ans = MAX(ans, i - arr[mask]);
     }
     return ans;
 }
