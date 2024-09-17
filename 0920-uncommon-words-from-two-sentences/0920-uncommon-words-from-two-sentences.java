bc9d6aeb33e6e7fc6031159db48de542810aa6ed
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-17 14:05:33 +0700

    Time: 5 ms (20.24%), Space: 41.9 MB (69.28%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,8 +1,8 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        StringBuilder ss = new StringBuilder(s1 + " " + s2);
+        String[] wrds = new StringBuilder(s1 + " " + s2).toString().split(" ");
         HashMap<String, Integer> mp = new HashMap<>();
-        for (String wrd: ss.toString().split(" ")) mp.put(wrd, mp.getOrDefault(wrd, 0) + 1);
+        for (String wrd: wrds) mp.put(wrd, mp.getOrDefault(wrd, 0) + 1);
         List<String> ans = new LinkedList<>();
         for (String wrd: mp.keySet()) 
             if (mp.get(wrd) == 1) ans.add(wrd);
