3a34cd1f56d3f2d43ce3f98337cd216d43cc0c31
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-21 21:35:51 +0700

    Time: 6 ms (78.34%), Space: 14.4 MB (40.61%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    vector<int> ans;
+    void genNums(int curr, int lim) {
+        if (curr > lim) return;
+
+        ans.push_back(curr);
+
+        for (int next = 0; next <= 9; ++next) {
+            int nextNum = curr * 10 + next;
+            if (nextNum <= lim) genNums(nextNum, lim);
+            else break;            
+        }
+    }
+public:
+    vector<int> lexicalOrder(int n) {
+        for (int st = 1; st <= 9; ++st) genNums(st, n);
+        return ans;
+    }
+};
