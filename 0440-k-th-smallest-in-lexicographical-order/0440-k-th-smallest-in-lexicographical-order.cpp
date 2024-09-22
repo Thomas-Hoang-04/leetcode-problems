0fd2a6e23731593ee880e8b7cafb3f5449b092d1
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-22 15:12:50 +0700

    Time: 3 ms (21.51%), Space: 7.5 MB (9.96%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        if (k-- == 1)
+            return 1;
+        int curr = 1;
+        long step;
+
+        while (k) {
+            step = 0;
+            for (long ref = 1, st = curr; st <= n; st *= 10, ref *= 10)
+                step += min(n + 1 - st, ref); 
+            if (step <= k) {
+                curr++;
+                k -= step;
+            }
+            else {
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
