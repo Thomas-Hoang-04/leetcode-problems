9069a85f9b08f59f29f2ed2da1cd109e57922e7a
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-22 15:11:32 +0700

    Time: 0 ms (100%), Space: 7.7 MB (100%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.c
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.c
@@ -1,25 +1,11 @@
-int min(long a, long b) {
-    return a < b ? a : b;
-}
-
-int cntSteps(int n, long pref) {
-    long max_pref = pref + 1;
-    int steps = 0;
-    while (pref <= n) {
-        steps += min((long)(n + 1), max_pref) - pref;
-        pref *= 10;
-        max_pref *= 10;
-    }
-    return steps;
-}
-
 int findKthNumber(int n, int k) {
-    if (k == 1) return 1;
-    int curr = 1, step; 
-    k--;
+    if (k-- == 1) return 1;
+    int curr = 1;
+    long step; 
 
     while (k) {
-        step = cntSteps(n, curr);
+        step = 0;
+        for (long ref = 1, st = curr; st <= n; st *= 10, ref *= 10) step += (n + 1 - st < ref) ? (n + 1 - st) : ref;
         if (step <= k) {
             curr++;
             k -= step;
