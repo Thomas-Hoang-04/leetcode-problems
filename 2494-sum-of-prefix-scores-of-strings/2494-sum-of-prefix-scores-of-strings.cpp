5feee5150a0ca8311a11e106b6375a3833cc22ee
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-09-25 14:09:12 +0700

    Time: 644 ms (30.92%), Space: 718.4 MB (46.63%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize(O3, "unroll-loops")
+
+struct TrieNode {
+    TrieNode* next[26] = {};
+    int cnt = 0;
+};
+
+class Solution {
+private:
+    TrieNode root;
+
+    void insert(string w) {
+        TrieNode* node = &root;
+        for (char c: w) {
+            if (!node->next[c - 'a']) node->next[c - 'a'] = new TrieNode();
+            node->next[c - 'a']->cnt++;
+            node = node->next[c - 'a'];
+        }
+    }
+
+    int count(string w) {
+        TrieNode* node = &root;
+        int ans = 0;
+        for (char c: w) {
+            ans +=  node->next[c - 'a']->cnt;
+            node = node->next[c - 'a'];
+        }
+        return ans;
+    }
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        for (int i = 0; i < words.size(); i++) insert(words[i]);
+        vector<int> scores(words.size(), 0);
+        for (int i = 0; i < words.size(); i++) scores[i] = count(words[i]);
+        return scores;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
