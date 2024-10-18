68a2f28c9847ece849e475fd07bf2578e54fb51f
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-10-18 10:56:49 +0700

    Time: 1 ms (88.89%), Space: 8 MB (22.22%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.c
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.c
@@ -1,18 +1,17 @@
 int countMaxOrSubsets(int* nums, int numsSize) {
-    int maxOr = *nums;
+    int maxOr = *nums, cnt = 0;
     for (int i = 1; i < numsSize; i++) maxOr |= nums[i];
 
-    int totalSub = 1 << numsSize;
-    int cnt = 0, curr;
-
-    for (int subMask = 1; subMask < totalSub; subMask++) {
-        curr = 0;
-
-        for (int i = 0; i < numsSize; i++) 
-            if ((subMask >> i) & 1) curr |= nums[i];
-        
-        cnt += curr == maxOr;
+    void subsets(int i, int curr) {
+        if (curr == maxOr) {
+            cnt += (1 << (numsSize - i));
+            return;
+        }
+        while (i < numsSize) {
+            subsets(i + 1, curr | nums[i]);
+            i++;
+        }
     }
-
+    subsets(0, 0);
     return cnt;
 }
