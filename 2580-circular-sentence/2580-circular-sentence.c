5fc80185c172e107676fd1dab863205fbef52c78
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-11-02 16:43:58 +0700

    Time: 0 ms (100%), Space: 7.9 MB (77.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.c
+++ 2580-circular-sentence/2580-circular-sentence.c
@@ -1,9 +1,12 @@
 bool isCircularSentence(char* se) {
     char* temp = se;
     while (*se) {
-        while (*se && *se != ' ') se++;
-        if (*se && *(se - 1) != *(se + 1)) return false;
-        if (*se) se++;
+        if (*se != ' ') {
+            se++;
+            continue;
+        }
+        if (*(se - 1) != *(se + 1)) return false;
+        se++;
     }
     return temp[0] == temp[strlen(temp) - 1];
 }
