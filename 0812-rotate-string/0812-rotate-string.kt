672516ce0a252c3d59ce2390ede99044278b25b8
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-11-03 17:06:50 +0700

    Time: 12 ms (7.14%), Space: 35.4 MB (29.49%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.kt
+++ 0812-rotate-string/0812-rotate-string.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun rotateString(s: String, goal: String): Boolean {
-        if (s == goal) return true;
+        if (s == goal) return true
         if (s.length != goal.length) return false
         return (s + s).contains(goal, true)
     }
