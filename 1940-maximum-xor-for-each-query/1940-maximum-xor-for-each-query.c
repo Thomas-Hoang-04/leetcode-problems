e8b9c85f598b858bb0f32cc9d93df7515ffdf37e
Hoàng Minh Hải <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-11-08 13:09:19 +0700

    Time: 0 ms (100%), Space: 84.8 MB (33.33%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.c
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.c
@@ -4,10 +4,9 @@
 int* getMaximumXor(int* nums, int numsSize, int maxBit, int* returnSize) {
     maxBit = (1 << maxBit) - 1;
     int* ans = (int*)malloc(numsSize * sizeof(int));
-    int idx = numsSize - 1;
     for (int i = 0; i < numsSize; i++) {
         if (i > 0) nums[i] ^= nums[i - 1];
-        ans[idx--] = nums[i] ^ maxBit;
+        ans[numsSize - i - 1] = nums[i] ^ maxBit;
     }
     *returnSize = numsSize;
     return ans;
